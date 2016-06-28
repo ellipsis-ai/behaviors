4c7a83b88275d538759d5ee03a24068c5a3d460e
Andrew Catton <andrew@catton.ca>
2016-06-28 19:40:00 -0400

    Add google behavior in Miscellaneous category

---
+++ published/Miscellaneous/google/function.js
@@ -0,0 +1,23 @@
+function(
+input,
+onSuccess, onError, ellipsis
+) {
+  var scraperjs = require('scraperjs');
+
+scraperjs.StaticScraper.create('https://www.google.com/search?q=' + input)
+	.scrape(function($) {
+		return $(".r a").map(function() {
+      var text = $(this).text();
+      var link = $(this).attr("href") || "";
+      link = link.replace(/\/url\?q=/, "");
+      link = link.replace(/&sa=.*/, "");
+			return  { link: link, text: text };
+		}).get();
+	})
+	.then(function(resultArray) {
+    var filtered = resultArray.filter(function(ea) { 
+      return !(/^\/search/.test(ea.link)); 
+    });
+  	onSuccess(filtered);
+	})
+}
---
+++ published/Miscellaneous/google/response.md
@@ -0,0 +1,5 @@
+Google results for "{input}":
+
+{for result in successResult}
+1. [{result.text}]({result.link})
+{endfor}
