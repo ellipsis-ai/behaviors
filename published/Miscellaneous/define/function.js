1cb940a2bec4d756a3ed0f208c77d7f9aac5e893
Andrew Catton <andrew@catton.ca>
2016-06-28 22:40:08 -0400

    publish define behavior

---
+++ published/Miscellaneous/define/function.js
@@ -0,0 +1,34 @@
+function(
+word,
+onSuccess, onError, ellipsis
+) {
+  var scraperjs = require('scraperjs');
+var dictionaryURL = "http://www.webster-dictionary.org/definition/";
+var qp = encodeURI(word);
+var requestURL = dictionaryURL + qp;
+
+scraperjs.StaticScraper.create(requestURL)
+  .scrape(function($) {
+    var mapContent = function() {
+      var text = $(this).text();
+      switch (this.tagName) {
+        case 'div':
+          return '\n\n' + $(this).contents().map(mapContent).get().join('') + '\n\n';
+        case 'b':
+          return '**' + text + '**';
+        default:
+          return text;
+      }
+    };
+
+    return $('tr').map(function() {
+      return $(this).find('td').map(function() {
+        return $(this).contents().map(mapContent).get().join('');
+      }).get().join(' ');
+    }).get();
+  })
+  .then(function(results) {
+    onSuccess(results);
+  });
+
+}
---
+++ published/Miscellaneous/define/response.md
@@ -0,0 +1,5 @@
+**{word}:**
+
+{for definition in successResult}
+{definition}
+{endfor}
