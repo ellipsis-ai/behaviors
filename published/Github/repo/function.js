91456601d4487b5966eb50e274a772afd31ac39c
Andrew Catton <andrew@catton.ca>
2016-10-20 20:51:27 -0400

    Trying some github stuff with data types

---
+++ published/Github/repo/function.js
@@ -0,0 +1,19 @@
+function(ellipsis) {
+  const GitHubApi = require("github");
+const github = new GitHubApi();
+
+github.authenticate({
+  type: "oauth",
+  token: ellipsis.accessTokens.github
+});
+
+github.repos.getAll({}, function(err, res) {
+  if (err) {
+    ellipsis.error(err.toString());
+  } else {
+    ellipsis.success(res.map((ea) => {
+      return { id: ea.full_name, label: ea.full_name };
+    }));
+  }
+});
+}
