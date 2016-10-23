755bd50548a4aa48397cb9edc5b7548f50255bb8
Andrew Catton <andrew@catton.ca>
2016-10-23 19:22:34 -0400

    use repo data type for open PRs behavior

--- published/Github/list-open-pull-requests/function.js
+++ published/Github/list-open-pull-requests/function.js
@@ -1,26 +1,27 @@
-function(
-repo,
-ellipsis
-) {
-  const fetch = require("fetch");
-const githubToken = ellipsis.accessTokens.github;
-const githubOwner = ellipsis.env.GITHUB_OWNER;
-const apiUrl = `https://api.github.com/repos/${githubOwner}/${repo}/pulls?access_token=${githubToken}&state=open`;
+function(repo, ellipsis) {
+  const GitHubApi = require("github");
+const github = new GitHubApi();
 
-fetch.fetchUrl(apiUrl, {}, (error, meta, body) => {
-  if (error) {
-    ellipsis.error(error);
-  } else if (meta.status == 200) {
-    const prs = JSON.parse(body.toString()).slice(0,9);
-    const msg = 
-          prs.length === 0 ? 
-            `No open PRs for the **${repo}** repo. Go for a walk or something.` : 
-            `PRs open at the moment for the **${repo}** repo:`;
-    ellipsis.success({ msg: msg, prs: prs }); 
-  } else {
-    ellipsis.error("Error: " + body.toString()); 
-  }
+github.authenticate({
+  type: "oauth",
+  token: ellipsis.accessTokens.github
 });
 
+const owner = repo.id.split("/")[0];
+const repoName = repo.id.split("/")[1];
+github.pullRequests.getAll({
+  owner: owner,
+  repo: repoName
+}, function(err, res) {
+  if (err) {
+    ellipsis.error(err.toString());
+  } else {
+    const msg =
+      res.length === 0 ?
+        `No open PRs for the **${repo.label}** repo. Go for a walk or something.` :
+        `PRs open at the moment for the **${repo.label}** repo:`;
+    ellipsis.success({ msg: msg, prs: res });
+  }
+});
 
 }
