395fcbf6cbfc702a440ea6b130e029e5cb5a9930
Andrew Catton <andrew@catton.ca>
2017-03-21 16:29:50 -0400

    added notification of non-answering users for classic standup; also included luke's validResponse() work

--- published/Classic_Standup/actions/Standup_status_summary/function.js
+++ published/Classic_Standup/actions/Standup_status_summary/function.js
@@ -9,8 +9,8 @@ function mostRecentByUserIn(arr) {
   const byUser = groupBy(arr, "userIdForContext");
   const userResults = [];
   Object.keys(byUser).forEach(key => {
-    const pv = byUser[key].sort((a, b) => a.timestamp > b.timestamp ? -1 : 1)[0].paramValues;
-    userResults.push({ user: key, today: pv.today, yesterday: pv.yesterday, blockers: pv.blockers });
+    var pv = byUser[key].sort((a, b) => a.timestamp > b.timestamp ? -1 : 1)[0].paramValues;
+    userResults.push(Object.assign({}, pv, {user: key}));
   });
   return userResults;
 }
@@ -28,17 +28,40 @@ function statusAnswers() {
   });
 }
 
+function usersAsked() {
+  return new Promise((resolve, reject) => {
+    getActionLogs({
+      action: "Check standup status",
+      from: from,
+      to: new Date(),
+      ellipsis: ellipsis,
+      success: resolve,
+      error: reject
+    });
+  });
+}
+
 const NO_RESPONSE = "_(no response yet)_";
  
-statusAnswers().then(response => {
-  const results = mostRecentByUserIn(response);
-  ellipsis.success(results.map(ea => {
-    return {
-      user: ea.user,
-      yesterday: (ea.yesterday ? ea.yesterday : NO_RESPONSE),
-      today: (ea.today ? ea.today : NO_RESPONSE),
-      blockers: (ea.blockers ? ea.blockers : NO_RESPONSE)
-    };
-  }));
-});
+usersAsked().then(usersAskedResponse => {
+  const askedByUser = mostRecentByUserIn(usersAskedResponse);
+  statusAnswers().then(response => {
+    const results = mostRecentByUserIn(response);
+    const answeredResults = results.map(ea => {
+      return {
+        user: ea.user,
+        yesterday: (ea.yesterday ? ea.yesterday : NO_RESPONSE),
+        today: (ea.today ? ea.today : NO_RESPONSE),
+        blockers: (ea.blockers ? ea.blockers : NO_RESPONSE)
+      };
+    });
+    const slackers = askedByUser.filter(eaAsked => {
+      return results.findIndex(eaAnswered => eaAnswered.user === eaAsked.user) === -1;
+    }).map(ea => ea.user);
+    ellipsis.success({
+      answeredResults: answeredResults,
+      slackers: slackers
+    });
+  });
+})
 }
--- published/Classic_Standup/actions/Standup_status_summary/response.md
+++ published/Classic_Standup/actions/Standup_status_summary/response.md
@@ -1,11 +1,17 @@
 Here's where everyone's at today:
 
-{for userResult in successResult}
+{for userResult in successResult.answeredResults}
 
 **<@{userResult.user}>:** 
 
 **Yesterday:** {userResult.yesterday}  
     **Today:** {userResult.today}  
- **Blockers:** {userResult.blockers}
+ **Blockers:** {userResult.blockers}  
 
+{endfor}
+
+The following slackers haven't answered yet:
+
+{for slacker in successResult.slackers}
+**<@{slacker}>**
 {endfor}
