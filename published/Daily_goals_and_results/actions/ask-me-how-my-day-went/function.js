3cb2bd533935b35013cad1db8d5dc34559e9ff0f
Andrew Catton <andrew@catton.ca>
2017-08-14 10:55:26 -0400

    Update the code to use built-in yes/no type

--- published/Daily_goals_and_results/actions/ask-me-how-my-day-went/function.js
+++ published/Daily_goals_and_results/actions/ask-me-how-my-day-went/function.js
@@ -1,7 +1,7 @@
 function(wasSuccessful, why, ellipsis) {
   var RandomResponse = require('ellipsis-random-response');
 ellipsis.success({
-  isSuccess: (wasSuccessful.label === "Yes"),
+  isSuccess: (wasSuccessful),
   successMessage: RandomResponse.responseWithEmoji("congratulatory"),
   failureMessage: RandomResponse.responseWithEmoji("oops")
 });
