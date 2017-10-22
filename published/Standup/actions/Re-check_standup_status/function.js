ff0b00d3842612d54a7162bc72589eadd83d7f51
Andrew Catton <andrew@catton.ca>
2017-10-22 11:29:08 -0400

    more specific text for the re-check

--- published/Standup/actions/Re-check_standup_status/function.js
+++ published/Standup/actions/Re-check_standup_status/function.js
@@ -3,6 +3,8 @@ function(channel, ellipsis) {
 const api = new EllipsisApi(ellipsis).actions;
 const ActionLogs = require('action-logs')(channel, ellipsis);
 const moment = require('moment-timezone');
+const RandomResponse = require('ellipsis-random-response');
+const greeting = RandomResponse.greetingForTimeZone(ellipsis.teamInfo.timeZone);
 
 const userId = ellipsis.ellipsisUserId;
 
@@ -13,8 +15,13 @@ ActionLogs.questionLogs(userId).then(questionLogs => {
       if (answerLogs.length) {
         ellipsis.noResponse(); // already answered
       } else {
-        const args = [{ name: "channel", value: channel }];
-        api.run({ actionName: "Check standup status", args: args }).then(response => ellipsis.noResponse());
+        api.say({ message: greeting }).then(response => {
+          api.say({message: `Just checking in again with standup questions for ${channel}` }).then(response => {
+            const name = "Answer status questions";
+            const args = [ { name: "channel", value: channel }];
+            api.run({ actionName: name, args: args }).then(response => ellipsis.noResponse());
+          });
+        });
       }
     });
   } else {
